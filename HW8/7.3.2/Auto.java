7dfed73c00c789ed87409f1eb5f0c1995e316dc9
dashaivanova1510-alt <dashaivanova1510@gmail.com>
2026-03-26 16:31:01 +0200

    Auto.java

---
+++ HW8/7.3.2/Auto.java
@@ -0,0 +1,13 @@
+public class Auto {
+    public int age;
+    public String model;
+    public boolean damaged; 
+    public int mileage;
+
+    public Auto(int age, String model, boolean damaged, int mileage) {
+        this.age = age;
+        this.model = model;
+        this.damaged = damaged;
+        this.mileage = mileage;
+    }
+}
