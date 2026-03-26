0a230d748c7af06ac69fd8ff2f76c27ddfadfa97
dashaivanova1510-alt <dashaivanova1510@gmail.com>
2026-03-26 17:09:24 +0200

    Manager.java

---
+++ HW10/Manager.java
@@ -0,0 +1,14 @@
+public class Manager implements Employee {
+    private final String name;
+    private final int salary;
+
+    public Manager(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public void payExpenses() {
+        System.out.println("Manager " + name + " has been paid $" + salary);
+    }
+}
